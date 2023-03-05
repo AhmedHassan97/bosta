d2bfabb55e185811c4d373c2df10718f950ef7ef
AhmedHassan97 <ahahmed202025@gmail.com>
2023-03-05 11:32:32 +0200

    chore: add mapping to arabic

---
+++ app/utilities/mapping-api-to-arabic.ts
@@ -0,0 +1,9 @@
+export const recordToArabic: Record<string, string> = {
+  TICKET_CREATED: "تم إنشاء التذكرة",
+  PACKAGE_RECEIVED: "تم استلام الطرد",
+  IN_TRANSIT: "في مرحلة انتقالية",
+  NOT_YET_SHIPPED: "لم يتم شحنه بعد",
+  OUT_FOR_DELIVERY: "خارج للتوصيل",
+  WAITING_FOR_CUSTOMER_ACTION: "في انتظار تصرف العميل",
+  DELIVERED: "تم التوصيل",
+};
